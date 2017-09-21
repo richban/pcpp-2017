3eec193410afa1a97450367fd7987d9fa610729e
Richard Banyi <developer2@Richards-MacBook-Pro.local>
2017-09-21 15:02:08 +0200

    exercise 3.4.3 however - java.lang.OutOfMemoryError: Java heap space

--- exercises/week3/Streams/stream.java
+++ exercises/week3/Streams/stream.java
@@ -2,6 +2,7 @@ import java.util.stream.*;
 
 public class Stream {
   static final int N = 999_999_999;
+  static double[] a = new double[N];
 
   public static void sumStream() {
     Long start = System.currentTimeMillis();
@@ -19,10 +20,24 @@ public class Stream {
     System.out.printf("Sum = %20.16f%n" + " and it took : "
                       + (end - start) + "ms", sum);
 
-    System.out.printf("Sum = %20.16f%n" + " and it took : "
+    System.out.printf("Sum2 = %20.16f%n" + " and it took : "
                       + (end2 - start2) + "ms with parallel", sum2);
+  }
+
+  public static void sumStandArray() {
+    double sum3 = 0;
+    Long start3 = System.currentTimeMillis();
+    for(int i = 0; i < a.length; i++){
+      a[i] = 1.0 / i;
+      sum3 += a[i];
+    }
+    Long end3 = System.currentTimeMillis();
+
+    System.out.printf("Sum3 = %20.16f%n" + " and it took : "
+                      + (end3 - start3) + "ms with for-loop", sum3);
   }
   public static void main(String[] args) {
     sumStream();
+    sumStandArray();
   }
 }
