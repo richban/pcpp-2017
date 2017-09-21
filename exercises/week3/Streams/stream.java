70832e8e677f25a6a92873e543a3613f6c984e4a
Richard Banyi <developer2@Richards-MacBook-Pro.local>
2017-09-21 14:17:27 +0200

    exercise 3.4.2

--- exercises/week3/Streams/stream.java
+++ exercises/week3/Streams/stream.java
@@ -1,13 +1,26 @@
 import java.util.stream.*;
 
-public class stream {
+public class Stream {
   static final int N = 999_999_999;
 
   public static void sumStream() {
+    Long start = System.currentTimeMillis();
     double sum = IntStream.range(1, N)
                           .mapToDouble(n -> 1.0 / n)
                           .sum();
-    System.out.printf("Sum = %20.16f%n", sum);
+    Long end = System.currentTimeMillis();
+
+    Long start2 = System.currentTimeMillis();
+    double sum2 = IntStream.range(1, N)
+                          .mapToDouble(n -> 1.0 / n)
+                          .sum();
+    Long end2 = System.currentTimeMillis();
+
+    System.out.printf("Sum = %20.16f%n" + " and it took : "
+                      + (end - start) + "ms", sum);
+
+    System.out.printf("Sum = %20.16f%n" + " and it took : "
+                      + (end2 - start2) + "ms with parallel", sum2);
   }
   public static void main(String[] args) {
     sumStream();
