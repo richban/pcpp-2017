7941fa5a20fdaf9711114c2969815adff5a58e30
Richard <rbanyi@me.com>
2018-01-28 18:23:57 +0100

    exam

--- exams/pcpp-20171211/QuickSelect/Timer.java
+++
@@ -1,9 +0,0 @@
-// Crude wall clock timing utility, measuring time in seconds
-   
-class Timer {
-  private long start = 0, spent = 0;
-  public Timer() { play(); }
-  public double check() { return (start==0 ? spent : System.nanoTime()-start+spent)/1e9; }
-  public void pause() { if (start != 0) { spent += System.nanoTime()-start; start = 0; } }
-  public void play() { if (start == 0) start = System.nanoTime(); }
-}
