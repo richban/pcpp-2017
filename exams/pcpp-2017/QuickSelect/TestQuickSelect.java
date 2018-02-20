a673e157dc4e7200cee7b091b48f610267999772
Richard <rbanyi@me.com>
2018-02-20 17:50:01 +0100

    implemented SortinBenchmarks

--- exams/pcpp-2017/QuickSelect/TestQuickSelect.java
+++ exams/pcpp-2017/QuickSelect/TestQuickSelect.java
@@ -40,7 +40,7 @@ class TestQuickSelect {
     int t=w[p]; w[p]=w[right]; w[right]=t;
     return right;
   }
-  
+
   public static int quickSelect(int[] inp) {
     int w[] = Arrays.copyOf(inp, inp.length);
     return quickSelect(w,0,w.length,w.length/2);
@@ -80,7 +80,7 @@ class TestQuickSelect {
       int j=0;
       for(int i=1;i<n;i++) if(inp[i]>=p) m[j++]=inp[i];
       return quickCountRec(m,target-count-1);
-    }    
+    }
     return p; // we are on target
   }
 
@@ -111,102 +111,120 @@ class TestQuickSelect {
     } while( true );
     return p; // we are on target
   }
-  
+
   public static void main( String [] args ) {
     SystemInfo();
-    int a[] = new int[Integer.parseInt(args[0])]; //100_000_000];
-    Random rnd = new Random();
-    if( args.length == 1 ) {
-      int nrIt = 10;
-      for(int ll=0;ll<nrIt;ll++) {
-        rnd.setSeed(23434+ll); // seed
-        for(int i=0;i<a.length;i++) a[i] = rnd.nextInt(4*a.length);
-        final int ra = quickCountRec(a,a.length/2); //
-        final int rb = medianPSort(a);
-        if( ra !=rb ) { 
-          System.out.println(ll);
-          System.out.println(ra);
-          System.out.println(rb);
-          System.exit(0);
-        }
-      }
-      System.out.println();
-    } else {
-        rnd.setSeed(23434+Integer.parseInt(args[1])); // seed
-        for(int i=0;i<a.length;i++) a[i] = rnd.nextInt(4*a.length);
-        System.out.println(medianPSort(a));
-        System.out.println(quickCountRec(a,a.length/2));
-    }
+    int input_size = Integer.parseInt(args[0]);
+    SortingBenchmarks(input_size);
+    // int a[] = new int[input_size]; //100_000_000];
+    // Random rnd = new Random();
+    // if( args.length == 1 ) {
+    //   int nrIt = 10;
+    //   for(int ll=0;ll<nrIt;ll++) {
+    //     rnd.setSeed(23434+ll); // seed
+    //     for(int i=0;i<a.length;i++) a[i] = rnd.nextInt(4*a.length);
+    //     final int ra = quickCountRec(a,a.length/2); //
+    //     final int rb = medianPSort(a);
+    //     if( ra !=rb ) {
+    //       System.out.println(ll);
+    //       System.out.println(ra);
+    //       System.out.println(rb);
+    //       System.exit(0);
+    //     }
+    //   }
+    //   System.out.println();
+    // } else {
+    //     rnd.setSeed(23434+Integer.parseInt(args[1])); // seed
+    //     for(int i=0;i<a.length;i++) a[i] = rnd.nextInt(4*a.length);
+    //     System.out.println(medianPSort(a));
+    //     System.out.println(quickCountRec(a,a.length/2));
+    // }
     //    System.exit(0);
-    double d=0.0;
-    d += Mark9("serial sort", a.length, x -> medianSort(a));
-    d += Mark9("parall sort", a.length, x -> medianPSort(a));
-    d += Mark9("serial qsel", a.length, x -> quickSelect(a));
-    d += Mark9("ser countRc", a.length,x -> quickCountRec(a,a.length/2));
-    d += Mark9("ser countIt", a.length,x -> quickCountIt(a));
+    // double d=0.0;
+    // d += Mark9("serial sort", a.length, x -> medianSort(a));
+    // d += Mark9("parall sort", a.length, x -> medianPSort(a));
+    // d += Mark9("serial qsel", a.length, x -> quickSelect(a));
+    // d += Mark9("ser countRc", a.length,x -> quickCountRec(a,a.length/2));
+    // d += Mark9("ser countIt", a.length,x -> quickCountIt(a));
     //d += Mark9("task countt", a.length,x -> quickCountItTask(a));
     //d += Mark9("task countR", a.length,x -> quickCountRecTask(a,a.length/2));
-    System.out.println(d);
+    // System.out.println(d);
   }
 
-    public static double Mark7(String msg, IntToDoubleFunction f) {
-    int n = 10, count = 1, totalCount = 0;
+  private static void SetupSort(int[] a) {
+    Random rnd = new Random();
+    int nrIt = 10;
+    for(int ll=0;ll<nrIt;ll++) {
+      rnd.setSeed(23434+ll); // seed
+      for(int i=0;i<a.length;i++) a[i] = rnd.nextInt(4*a.length);
+      final int ra = quickCountRec(a,a.length/2); //
+      final int rb = medianPSort(a);
+      if( ra !=rb ) {
+        System.out.println(ll);
+        System.out.println(ra);
+        System.out.println(rb);
+        System.exit(0);
+      }
+    }
+  }
+
+  private static void SortingBenchmarks(int input_size) {
+    for (int size = 100; size <= input_size; size *= 2) {
+      final int a[] = new int[size];
+      Mark8Setup("serial sort",
+                 String.format("%8d", size),
+                 new Benchmarkable() {
+                   public void setup() { SetupSort(a); }
+                   public double applyAsDouble(int i)
+                   { medianSort(a); return 0.0; } });
+    }
+  }
+
+  public static double Mark8Setup(String msg, String info, Benchmarkable f,
+                                  int n, double minTime) {
+    int count = 1, totalCount = 0;
     double dummy = 0.0, runningTime = 0.0, st = 0.0, sst = 0.0;
-    do { 
+    do {
       count *= 2;
       st = sst = 0.0;
       for (int j=0; j<n; j++) {
         Timer t = new Timer();
-        for (int i=0; i<count; i++) 
+        for (int i=0; i<count; i++) {
+          t.pause();
+          f.setup();
+          t.play();
           dummy += f.applyAsDouble(i);
+        }
         runningTime = t.check();
         double time = runningTime * 1e9 / count;
-        st += time; 
+        st += time;
         sst += time * time;
         totalCount += count;
       }
-    } while (runningTime < 0.25 && count < Integer.MAX_VALUE/2);
+    } while (runningTime < minTime && count < Integer.MAX_VALUE/2);
     double mean = st/n, sdev = Math.sqrt((sst - mean*mean*n)/(n-1));
-    System.out.printf("%-25s %15.1f ns %10.2f %10d%n", msg, mean, sdev, count);
+    System.out.printf("%-25s %s%15.1f ns %10.2f %10d%n", msg, info, mean, sdev, count);
     return dummy / totalCount;
   }
 
-  public static double Mark9(String msg, int size, IntToDoubleFunction f) {
-    int n = 5, count = 1, totalCount = 0;
-    double dummy = 0.0, runningTime = 0.0, st = 0.0, sst = 0.0;
-    do { 
-      count *= 2;
-      st = sst = 0.0;
-      for (int j=0; j<n; j++) {
-        Timer t = new Timer();
-        for (int i=0; i<count; i++) 
-          dummy += f.applyAsDouble(i);
-        runningTime = t.check();
-        double time = runningTime * 1e9 / count; // microseconds
-        st += time; 
-        sst += time * time;
-        totalCount += count;
-      }
-    } while (runningTime < 0.25 && count < Integer.MAX_VALUE/2);
-    double mean = st/n/size, sdev = Math.sqrt((sst - mean*mean*n)/(n-1))/size;
-    System.out.printf("%-25s %15.1f ns %10.2f %10d%n", msg, mean, sdev, count);
-    return dummy / totalCount;
+  public static double Mark8Setup(String msg, String info, Benchmarkable f) {
+    return Mark8Setup(msg, info, f, 10, 0.25);
   }
 
   public static void SystemInfo() {
-    System.out.printf("# OS:   %s; %s; %s%n", 
-                      System.getProperty("os.name"), 
-                      System.getProperty("os.version"), 
+    System.out.printf("# OS:   %s; %s; %s%n",
+                      System.getProperty("os.name"),
+                      System.getProperty("os.version"),
                       System.getProperty("os.arch"));
-    System.out.printf("# JVM:  %s; %s%n", 
-                      System.getProperty("java.vendor"), 
+    System.out.printf("# JVM:  %s; %s%n",
+                      System.getProperty("java.vendor"),
                       System.getProperty("java.version"));
     // The processor identifier works only on MS Windows:
-    System.out.printf("# CPU:  %s; %d \"cores\"%n", 
+    System.out.printf("# CPU:  %s; %d \"cores\"%n",
                       System.getenv("PROCESSOR_IDENTIFIER"),
                       Runtime.getRuntime().availableProcessors());
     java.util.Date now = new java.util.Date();
-    System.out.printf("# Date: %s%n", 
+    System.out.printf("# Date: %s%n",
       new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").format(now));
   }
 
