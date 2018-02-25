b03ccfab5eb474b45f59764b87da614d8922da9d
Davide Laezza <truzzialrogo@gmx.com>
2018-02-25 20:35:53 +0100

    Week 12 exercises

--- exercises/week12/ABC.java
+++ exercises/week12/ABC.java
@@ -1,130 +1,152 @@
-import java.util.Random; import java.io.*; import akka.actor.*;
+import java.util.Iterator;
+import java.util.Random;
+import java.io.*;
+import akka.actor.*;
 
-// -- MESSAGES --------------------------------------------------
+// class MESSAGES --------------------------------------------------
+
 class StartTransferMessage implements Serializable {
-	public final ActorRef Bank;
-	public final ActorRef From;
-	public final ActorRef To;
-	public StartTransferMessage()
-	{
-		Bank = null;
-		From = null;
-		To = null;
-	}
-	
-	public StartTransferMessage(ActorRef bank,ActorRef from, ActorRef to)
-	{
-		Bank = bank;
-		From = from;
-		To = to;
-	} 
-}
-class TransferMessage implements Serializable { 
-	public final int Amount;
-	public final ActorRef From;
-	public final ActorRef To;
-	public TransferMessage()
-	{
-		Amount = 0;
-		From = null;
-		To = null;
-	}
-	public TransferMessage(int amount,ActorRef from, ActorRef to)
-	{
-		Amount = amount;
-		From = from;
-		To = to;
-	}
- }
-class DepositMessage implements Serializable { 
-	public final int Amount;
-	public DepositMessage()
-	{
-		Amount = 0; 
-	}
-	public DepositMessage(int amount)
-	{
-		Amount = amount;
-	}
-}
-class PrintBalanceMessage implements Serializable { }
+    public final ActorRef bank;
+    public final ActorRef from;
+    public final ActorRef to;
 
-// -- ACTORS --------------------------------------------------
+    public StartTransferMessage(ActorRef bank, ActorRef from, ActorRef to) {
+        this.bank = bank;
+        this.from = from;
+        this.to = to;
+    }
+}
+
+class TransferMessage implements Serializable {
+    public final double amount;
+    public final ActorRef from;
+    public final ActorRef to;
+
+    public TransferMessage(double amount, ActorRef from, ActorRef to) {
+        this.amount = amount;
+        this.from = from;
+        this.to = to;
+    }
+}
+
+class DepositMessage implements Serializable {
+    public final double amount;
+
+    public DepositMessage(double amount) {
+        this.amount = amount;
+    }
+}
+
+class PrintBalanceMessage implements Serializable {}
+
+// class ACTORS --------------------------------------------------
+
 class AccountActor extends UntypedActor {
-	private int Balance;
-	public void onReceive(Object o) throws Exception {
-		 if (o instanceof PrintBalanceMessage) { 
-			 System.out.format("Balance = %d\n",Balance);
-		 } else 
-		 if (o instanceof DepositMessage) {
-			 DepositMessage message = (DepositMessage) o;
-			 Balance += message.Amount;
-		 } 
-	 }
- }
-class BankActor extends UntypedActor {
-	public void onReceive(Object o) throws Exception {
-		 if (o instanceof TransferMessage) {  		
-			TransferMessage message = (TransferMessage) o;	  
-			message.From.tell(new DepositMessage(-message.Amount), ActorRef.noSender());  
-			message.To.tell(new DepositMessage(message.Amount), ActorRef.noSender());  
-		 } 
-	 } 
- }
-class ClerkActor extends UntypedActor { 	  
-	void NTransfer(Random rand, int n, ActorRef bank, ActorRef from, ActorRef to)
-	{
-		for (int i = 0; i < n; i++)
-		{
-			int randomAmount = rand.nextInt(n);
-			bank.tell(new TransferMessage(randomAmount,from,to), ActorRef.noSender());  
-		}
-	}
-	public void onReceive(Object o) throws Exception {
-		 if (o instanceof StartTransferMessage) {  
-			 long seed = System.currentTimeMillis() + this.hashCode();
-			 Random rand = new Random(seed);
-			 StartTransferMessage message = (StartTransferMessage) o;
-			 NTransfer(rand, 100, message.Bank,message.From,message.To);
-		 } 
-	 }
+    private static long nextID = 0;
 
- }
+    private long ID;
+    private double balance;
+
+    public AccountActor() {
+        ID = AccountActor.nextID++;
+    }
+
+    public void onReceive(Object m)
+            throws Exception {
+        if (m instanceof DepositMessage) {
+            balance += ((DepositMessage) m).amount;
+        }
+        else if (m instanceof PrintBalanceMessage) {
+            System.out.printf("Account %d - balance: %f\n", ID, balance);
+        }
+    }
+}
+
+class BankActor extends UntypedActor {
+    public void onReceive(Object m)
+            throws Exception {
+        if (m instanceof TransferMessage) {
+            TransferMessage msg = (TransferMessage) m;
+            msg.from.tell(new DepositMessage(-msg.amount), ActorRef.noSender());
+            msg.to.tell(new DepositMessage(+msg.amount), ActorRef.noSender());
+        }
+    }
+}
+
+class ClerkActor extends UntypedActor {
+    private static final int SPAMS_COUNT = 100;
+
+    private static void spamBank(ActorRef bank, ActorRef from, ActorRef to) {
+        long seed = System.nanoTime() + Thread.currentThread().hashCode();
+        Iterator<Double> iterator = new Random(seed)
+            .doubles(ClerkActor.SPAMS_COUNT)
+            .iterator();
+
+        while (iterator.hasNext()) {
+            double amount = iterator.next();
+            bank.tell(new TransferMessage(amount, from, to), ActorRef.noSender());
+        }
+    }
+
+    public void onReceive(Object m)
+            throws Exception {
+        if (m instanceof StartTransferMessage) {
+            StartTransferMessage msg = (StartTransferMessage) m;
+            ClerkActor.spamBank(msg.bank, msg.from, msg.to);
+        }
+    }
+}
 
 // -- MAIN --------------------------------------------------
+
 public class ABC { // Demo showing how things work:
- public static void main(String[] args) throws Exception {
-	 final ActorSystem system = ActorSystem.create("ABCSystem");
-	 /* TODO (CREATE ACTORS AND SEND START MESSAGES) */
-	 final ActorRef a1 =
-		system.actorOf(Props.create(AccountActor.class), "account1");
-	final ActorRef a2 =
-		system.actorOf(Props.create(AccountActor.class), "account2");
-	 final ActorRef b1 =
-		system.actorOf(Props.create(BankActor.class), "bank1"); 
-	 final ActorRef b2 =
-		system.actorOf(Props.create(BankActor.class), "bank2"); 
-	 final ActorRef c1 =
-		system.actorOf(Props.create(ClerkActor.class), "clerk1"); 
-	 final ActorRef c2 =
-		system.actorOf(Props.create(ClerkActor.class), "clerk2"); 
-		
-	 try {		 
-		 c1.tell(new StartTransferMessage(b1,a1,a2), ActorRef.noSender()); 
-		 c2.tell(new StartTransferMessage(b2,a2,a1), ActorRef.noSender()); 
-		 System.out.println("Press return to inspect...");
-		 System.in.read();
-		 a1.tell(new PrintBalanceMessage(), ActorRef.noSender()); 
-		 a2.tell(new PrintBalanceMessage(), ActorRef.noSender()); 
-		 Thread.sleep(1000);
-		 
-		 /* TODO (INSPECT FINAL BALANCES) */
-		 System.out.println("Press return to terminate...");
-		 System.in.read();
-	 } catch(IOException e) {
-		e.printStackTrace();
-	 } finally {
-		system.shutdown();
-	 }
- }
-} 
+    public static void main(String[] args) {
+        final ActorSystem system = ActorSystem.create("ABCSystem");
+
+        // Actors creation
+
+        final ActorRef[] accounts = new ActorRef[] {
+                system.actorOf(Props.create(AccountActor.class), "account0"),
+                system.actorOf(Props.create(AccountActor.class), "account1")
+            };
+
+        final ActorRef[] banks = new ActorRef[] {
+                system.actorOf(Props.create(BankActor.class), "bank0"),
+                system.actorOf(Props.create(BankActor.class), "bank1")
+            };
+
+        final ActorRef[] clerks = new ActorRef[] {
+                system.actorOf(Props.create(ClerkActor.class), "clerk0"),
+                system.actorOf(Props.create(ClerkActor.class), "clerk1")
+            };
+
+        // Sending start messages
+
+        for (int k = 0; k < clerks.length; ++k) {
+            StartTransferMessage msg = new StartTransferMessage(
+                banks[k],
+                accounts[k],
+                accounts[accounts.length - k - 1]
+            );
+            clerks[k].tell(msg, ActorRef.noSender());
+        }
+
+        try {
+            System.out.println("Press return to inspect...");
+            System.in.read();
+
+            for (ActorRef account : accounts) {
+                account.tell(new PrintBalanceMessage(), ActorRef.noSender());
+            }
+
+            System.out.println("Press return to terminate...");
+            System.in.read();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            system.shutdown();
+        }
+    }
+}
