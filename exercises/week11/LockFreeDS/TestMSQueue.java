f67e0e8a207a90d32f4d2af6330391af0f22af8e
Richard <rbanyi@me.com>
2018-12-13 22:53:59 +0100

    michaels-scott queue

--- exercises/week11/LockFreeDS/TestMSQueue.java
+++ exercises/week11/LockFreeDS/TestMSQueue.java
@@ -6,12 +6,135 @@
 
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class TestMSQueue {
-  public static void main(String[] args) {
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.CyclicBarrier;
+
+
+public class TestMSQueue extends Tests{
+  public static void main(String[] args) throws Exception {
+    sequentialTest(new MSQueue<Integer>());
+    parallelTest(new MSQueue<Integer>(), 10);
+  }
+
+  private static void sequentialTest(UnboundedQueue<Integer> bq) throws Exception {
+    System.out.printf("%nSequential test: %s", bq.getClass());
+    assertTrue(bq.dequeue() == null);
+    bq.enqueue(7); bq.enqueue(9); bq.enqueue(13);
+    assertEquals(bq.dequeue(), 7);
+    assertEquals(bq.dequeue(), 9);
+    assertEquals(bq.dequeue(), 13);
+    assertTrue(bq.dequeue() == null);
+    System.out.println("... passed");
+  }
+
+  private static void parallelTest(UnboundedQueue<Integer> bq, int nThreads) throws Exception {
+    long startTime = System.currentTimeMillis();
+    System.out.printf("%nParallel test: %s", bq.getClass());
+    final ExecutorService pool = Executors.newCachedThreadPool();
+    new PutTakeTest(bq, nThreads, 100000).test(pool);
+    pool.shutdown();
+    System.out.println("... passed");
+    long endTime   = System.currentTimeMillis();
+    long totalTime = endTime - startTime;
+    System.out.println(totalTime);
   }
 }
 
+class Tests {
+  public static void assertEquals(int x, int y) throws Exception {
+    if (x != y)
+      throw new Exception(String.format("ERROR: %d not equal to %d%n", x, y));
+  }
+
+  public static void assertTrue(boolean b) throws Exception {
+    if (!b)
+      throw new Exception(String.format("ERROR: assertTrue"));
+  }
+}
+
+/**
+ * Producer-consumer test program for BoundedQueue
+ *
+ * @author Brian Goetz and Tim Peierls; some modifications by
+ * sestoft@itu.dk
+ */
+
+class PutTakeTest extends Tests {
+  // We could use one CyclicBarrier for both starting and stopping,
+  // precisely because it is cyclic, but the code becomes clearer by
+  // separating them:
+  protected CyclicBarrier startBarrier, stopBarrier;
+  protected final UnboundedQueue<Integer> bq;
+  protected final int nTrials, nPairs;
+  protected final AtomicInteger putSum = new AtomicInteger(0);
+  protected final AtomicInteger takeSum = new AtomicInteger(0);
+
+  public PutTakeTest(UnboundedQueue<Integer> bq, int npairs, int ntrials) {
+    this.bq = bq;
+    this.nTrials = ntrials;
+    this.nPairs = npairs;
+    this.startBarrier = new CyclicBarrier(npairs * 2 + 1);
+    this.stopBarrier = new CyclicBarrier(npairs * 2 + 1);
+  }
+
+  void test(ExecutorService pool) {
+    try {
+      for (int i = 0; i < nPairs; i++) {
+        pool.execute(new Producer());
+        pool.execute(new Consumer());
+      }
+      startBarrier.await(); // wait for all threads to be ready
+      stopBarrier.await();  // wait for all threads to finish
+      assertTrue(bq.dequeue() == null);
+      assertEquals(putSum.get(), takeSum.get());
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  class Producer implements Runnable {
+    public void run() {
+      try {
+        Random random = new Random();
+        int sum = 0;
+        startBarrier.await();
+        for (int i = nTrials; i > 0; --i) {
+          int item = random.nextInt();
+          bq.enqueue(item);
+          sum += item;
+        }
+        putSum.getAndAdd(sum);
+        stopBarrier.await();
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+
+  class Consumer implements Runnable {
+    public void run() {
+      try {
+        startBarrier.await();
+        int sum = 0;
+        for (int i = nTrials; i > 0; --i) {
+          Integer item = null;
+          while (item == null) {
+            item = bq.dequeue();
+          }
+          sum += item;
+        }
+        takeSum.getAndAdd(sum);
+        stopBarrier.await();
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+}
 
 interface UnboundedQueue<T> {
   void enqueue(T item);
@@ -21,17 +144,17 @@ interface UnboundedQueue<T> {
 // ------------------------------------------------------------
 // Unbounded lock-based queue with sentinel (dummy) node
 
-class LockingQueue<T> implements UnboundedQueue<T> {  
+class LockingQueue<T> implements UnboundedQueue<T> {
   // Invariants:
   // The node referred by tail is reachable from head.
-  // If non-empty then head != tail, 
+  // If non-empty then head != tail,
   //    and tail points to last item, and head.next to first item.
   // If empty then head == tail.
 
   private static class Node<T> {
     final T item;
     Node<T> next;
-    
+
     public Node(T item, Node<T> next) {
       this.item = item;
       this.next = next;
@@ -43,7 +166,7 @@ class LockingQueue<T> implements UnboundedQueue<T> {
   public LockingQueue() {
     head = tail = new Node<T>(null, null);
   }
-  
+
   public synchronized void enqueue(T item) { // at tail
     Node<T> node = new Node<T>(item, null);
     tail.next = node;
@@ -51,7 +174,7 @@ class LockingQueue<T> implements UnboundedQueue<T> {
   }
 
   public synchronized T dequeue() {     // from head
-    if (head.next == null) 
+    if (head.next == null)
       return null;
     Node<T> first = head;
     head = first.next;
@@ -61,7 +184,7 @@ class LockingQueue<T> implements UnboundedQueue<T> {
 
 
 // ------------------------------------------------------------
-// Unbounded lock-free queue (non-blocking in M&S terminology), 
+// Unbounded lock-free queue (non-blocking in M&S terminology),
 // using CAS and AtomicReference
 
 // This creates one AtomicReference object for each Node object.  The
@@ -83,7 +206,7 @@ class MSQueue<T> implements UnboundedQueue<T> {
     Node<T> node = new Node<T>(item, null);
     while (true) {
       Node<T> last = tail.get(), next = last.next.get();
-      if (last == tail.get()) {         // E7
+      // if (last == tail.get()) {         // E7
         if (next == null)  {
           // In quiescent state, try inserting new node
           if (last.next.compareAndSet(next, node)) { // E9
@@ -91,11 +214,11 @@ class MSQueue<T> implements UnboundedQueue<T> {
             tail.compareAndSet(last, node);
             return;
           }
-        } else 
+        } else
           // Queue in intermediate state, advance tail
           tail.compareAndSet(last, next);
       }
-    }
+    // }
   }
 
   public T dequeue() { // from head
@@ -142,12 +265,12 @@ class MSQueueRefl<T> implements UnboundedQueue<T> {
     tail = new AtomicReference<Node<T>>(dummy);
   }
 
-  @SuppressWarnings("unchecked") 
+  @SuppressWarnings("unchecked")
   // Java's @$#@?!! generics type system: abominable unsafe double type cast
-  private final AtomicReferenceFieldUpdater<Node<T>, Node<T>> nextUpdater 
-    = AtomicReferenceFieldUpdater.newUpdater((Class<Node<T>>)(Class<?>)(Node.class), 
-                                             (Class<Node<T>>)(Class<?>)(Node.class), 
-                                             "next");    
+  private final AtomicReferenceFieldUpdater<Node<T>, Node<T>> nextUpdater
+    = AtomicReferenceFieldUpdater.newUpdater((Class<Node<T>>)(Class<?>)(Node.class),
+                                             (Class<Node<T>>)(Class<?>)(Node.class),
+                                             "next");
 
   public void enqueue(T item) { // at tail
     Node<T> node = new Node<T>(item, null);
@@ -168,7 +291,7 @@ class MSQueueRefl<T> implements UnboundedQueue<T> {
       }
     }
   }
-  
+
   public T dequeue() { // from head
     while (true) {
       Node<T> first = head.get(), last = tail.get(), next = first.next;
