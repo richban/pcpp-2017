6a3e1d770ef463179fdbf1eb364d2f46ebc8c44a
Richard <rbanyi@me.com>
2018-12-18 21:34:05 +0100

    use threads instead of tasks - it's unnecesarry

--- exams/pcpp-2018/bufferedMergePQ/ParallelPQPair.java
+++ exams/pcpp-2018/bufferedMergePQ/ParallelPQPair.java
@@ -1,59 +1,25 @@
 import java.util.function.Function;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Future;
 import java.util.List;
 import java.util.ArrayList;
 
 
 public class ParallelPQPair implements PQPair {
-  private static final ExecutorService executor = Executors.newCachedThreadPool();
-  // Cast the object to its class type
-  ThreadPoolExecutor pool = (ThreadPoolExecutor) executor;
   PQ left, right;
 
   public PQPair clone() {
       return new ParallelPQPair();
   }
   public void createPairParam(Parameters param, Function<Parameters,PQ> instanceCreator){
-    List<Callable<PQ>> tasks = new ArrayList<Callable<PQ>>();
-
-    //Stats before tasks execution
-    // System.out.println("Largest executions: " + pool.getLargestPoolSize());
-    // System.out.println("Maximum allowed threads: " + pool.getMaximumPoolSize());
-    // System.out.println("Current threads in pool: " + pool.getPoolSize());
-    // System.out.println("Currently executing threads: " + pool.getActiveCount());
-    // System.out.println("Total number of threads(ever scheduled): " + pool.getTaskCount());
-
-    // Create a task for the left data structure
-    tasks.add(() -> {
-      // System.out.println("Running Task! Thread Name: " + Thread.currentThread().getName());
-      return instanceCreator.apply( param.left());
+    Thread t1 = new Thread(() -> {
+      left = instanceCreator.apply( param.left());
     });
-    // Create a task for the right data structure
-    tasks.add(() -> {
-      // System.out.println("Running Task! Thread Name: " + Thread.currentThread().getName());
-      return instanceCreator.apply( param.right());
+    Thread t2 = new Thread(() -> {
+      right = instanceCreator.apply( param.right());
     });
-
+    t1.start(); t2.start();
     try {
-      List<Future<PQ>> futures = executor.invokeAll(tasks);
-      // Wait for all tasks to complete
-      left = futures.get(0).get();
-      right = futures.get(1).get();
-
-      //Stats after tasks execution
-      // System.out.println("Core threads: " + pool.getCorePoolSize());
-      // System.out.println("Largest executions: " + pool.getLargestPoolSize());
-      // System.out.println("Maximum allowed threads: " + pool.getMaximumPoolSize());
-      // System.out.println("Current threads in pool: " + pool.getPoolSize());
-      // System.out.println("Currently executing threads: " + pool.getActiveCount());
-      // System.out.println("Total number of threads(ever scheduled): " + pool.getTaskCount());
-
-    } catch (InterruptedException | ExecutionException exn) {
+      t1.join(); t2.join();
+    } catch (InterruptedException exn) {
       System.out.println("Interrupted: " + exn);
     }
 
